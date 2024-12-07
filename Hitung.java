785c68b61ba43da340a3e559c7341cad59939e1d
rahmanfaisal653 <rahmanfaisal653@gmail.com>
2024-12-07 07:10:32 +0700

    Update Hitung.java

--- Hitung.java
+++ Hitung.java
@@ -1,14 +1,59 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 
-public class Hitung{
+public class Hitung {
 
     public static ArrayList<Integer> data = new ArrayList<>();
-    public static void main(String[] args){
+
+    public static int minimal(ArrayList<Integer> list) {
+        int min = list.get(0);
+        for (Integer angka : list) {
+            if (angka < min) {
+                min = angka;
+            }
+        }
+        return min;
+    }
+
+    public static int maximal(ArrayList<Integer> list) {
+        int max = list.get(0);
+        for (Integer angka : list) {
+            if (angka > max) {
+                max = angka;
+            }
+        }
+        return max;
+    }
+
+    public static int ratarata(ArrayList<Integer> list) {
+       int rata = 0;
+       for (int angka : list) {
+            rata += angka;
+       }
+       return rata / list.size();
+    }
+
+    
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
         /* Pekerjaan anda mulai dari sini */
 
+        String pilihan;
+        do {
+            int dataInput = input.nextInt();
+            data.add(dataInput);
+            System.out.println("Input lagi? Y/T");
+            pilihan = input.next();
+
+        } while (pilihan.equalsIgnoreCase("Y"));
+
+        System.out.println("min = " + minimal(data));
+        System.out.println("max = " + maximal(data));
+        System.out.println("rata = " + ratarata(data));
+
         /* Pekerjaan anda berakhir sini */
+
         System.out.println(Hitung.class.getDeclaredMethods().length);
         System.out.println(Hitung.class.getDeclaredFields().length);
     }
